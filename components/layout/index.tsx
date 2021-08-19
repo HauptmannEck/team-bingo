cfffe7e736c1227aae3e4d23d49f18cd381fe0e8
HauptmannEck <eckhoffj@gmail.com>
2021-08-18 19:33:52 -0500

    Add App Bar and Delete

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -1,15 +1,32 @@
-import React from "react";
-import styles from './index.module.scss'
+import React from 'react';
+import { AppBar, Container, IconButton, Toolbar } from '@material-ui/core';
+import HomeIcon from '@material-ui/icons/Home';
+import { useRouter } from 'next/router';
 
 interface Props {
-    home?: boolean;
+    toolbar?: JSX.Element;
 }
 
-const Layout: React.FC<Props> = ({children, home = false}) => (
-    <div className={styles.container}>
-        {children}
-        <footer>From Happy Bandit</footer>
-    </div>
-);
+const Layout: React.FC<Props> = ( { children, toolbar } ) => {
+    const router = useRouter();
+
+    return (
+        <>
+            <AppBar>
+                <Toolbar variant="dense">
+                    <IconButton edge="start" color="inherit" aria-label="menu" onClick={() => router.push('/')}>
+                        <HomeIcon/>
+                    </IconButton>
+                    {toolbar ?? ''}
+                </Toolbar>
+            </AppBar>
+            <Toolbar variant="dense"/>
+            <Container>
+                {children}
+                <footer>From Happy Bandit</footer>
+            </Container>
+        </>
+    );
+};
 
 export default Layout;
--- pages/game/index.tsx
+++ pages/game/index.tsx
@@ -1,37 +1,37 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import styles from '../index.module.scss';
 import Layout from '../../components/layout';
-import {Button, TextField} from '@material-ui/core';
-import {useRouter} from 'next/router';
+import { Button, TextField } from '@material-ui/core';
+import { useRouter } from 'next/router';
 
-const sendNewGame = async (name: string, adminCode: string) => {
-    const res = await fetch('api/create-game', {
-        body: JSON.stringify({
+const sendNewGame = async ( name: string, adminCode: string ) => {
+    const res = await fetch( 'api/game', {
+        body: JSON.stringify( {
             name,
             adminCode,
-        }),
+        } ),
         headers: {
-            'Content-Type': 'application/json'
+            'Content-Type': 'application/json',
         },
-        method: 'POST'
-    })
+        method: 'POST',
+    } );
 
-    if (res.status == 201) {
+    if ( res.status == 201 ) {
         return await res.json();
     } else {
-        throw Error(await res.text());
+        throw Error( await res.text() );
     }
-}
+};
 
 const Home: React.FC = () => {
-    const router = useRouter()
-    const [name, setName] = useState('');
-    const [adminCode, setAdminCode] = useState('');
+    const router = useRouter();
+    const [name, setName] = useState( '' );
+    const [adminCode, setAdminCode] = useState( '' );
 
     const handleCreate = () => {
-        sendNewGame(name, adminCode)
-            .then((row) => router.push(`/game/${row.uuid}`));
-    }
+        sendNewGame( name, adminCode )
+            .then( ( row ) => router.push( `/game/${row.uuid}` ) );
+    };
 
     return (
         <Layout>
@@ -41,18 +41,19 @@ const Home: React.FC = () => {
                 </h1>
 
                 <p className={styles.description}>
-                    Select the name of the new game and the admin code. Do not share your admin code as that gives full control of the game.
+                    Select the name of the new game and the admin code. Do not share your admin code as that gives full
+                    control of the game.
                 </p>
 
                 <TextField
                     value={name}
                     label="Name"
-                    onChange={(event) => setName(event.target.value)}
+                    onChange={( event ) => setName( event.target.value )}
                 />
                 <TextField
                     value={adminCode}
                     label="Admin Code"
-                    onChange={(event) => setAdminCode(event.target.value)}
+                    onChange={( event ) => setAdminCode( event.target.value )}
                 />
                 <Button
                     disabled={!name || !adminCode}
