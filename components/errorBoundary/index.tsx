1e52939a40b658f8dfaac5e6a5e0abd28317cd43
HauptmannEck <eckhoffj@gmail.com>
2021-11-09 17:48:40 -0600

    Fix logging and use new planetscale packag e that fixed queries.

--- components/errorBoundary/index.tsx
+++ components/errorBoundary/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
-import { Logtail } from "@logtail/browser";
+import { Logtail } from '@logtail/browser';
 
-const logger = new Logtail(process.env.NEXT_PUBLIC_LOGTAIL_SOURCE_TOKEN ?? '');
+let logger: Logtail | undefined;
+if ( process.env.NEXT_PUBLIC_LOGTAIL_SOURCE_TOKEN ) {
+    logger = new Logtail( process.env.NEXT_PUBLIC_LOGTAIL_SOURCE_TOKEN );
+}
 
-class ErrorBoundary extends React.Component<any, {hasError: boolean}> {
-    constructor(props: any) {
-        super(props);
+class ErrorBoundary extends React.Component<any, { hasError: boolean }> {
+    constructor( props: any ) {
+        super( props );
         this.state = { hasError: false };
     }
 
@@ -14,13 +17,15 @@ class ErrorBoundary extends React.Component<any, {hasError: boolean}> {
         return { hasError: true };
     }
 
-    componentDidCatch(error: string | Error, errorInfo: any) {
+    componentDidCatch( error: string | Error, errorInfo: any ) {
         // You can also log the error to an error reporting service
-        return logger.error(error, errorInfo);
+        if ( logger ) {
+            return logger.error( error, errorInfo );
+        }
     }
 
     render() {
-        if (this.state.hasError) {
+        if ( this.state.hasError ) {
             // You can render any custom fallback UI
             return <h1>Something went wrong.</h1>;
         }
--- utils/logServer.ts
+++ utils/logServer.ts
@@ -1,17 +1,23 @@
 import { Logtail } from '@logtail/node';
 import type {NextApiHandler, NextApiRequest, NextApiResponse} from 'next';
 
-const serverLogger = new Logtail(process.env.LOGTAIL_SOURCE_TOKEN || '');
+let serverLogger: Logtail | undefined;
+
+if ( process.env.LOGTAIL_SOURCE_TOKEN ) {
+    serverLogger = new Logtail( process.env.LOGTAIL_SOURCE_TOKEN );
+}
 
 export const errorHandler = (handler: NextApiHandler): NextApiHandler => (req: NextApiRequest, res: NextApiResponse) => {
     try {
         return handler(req, res);
     } catch (e) {
         console.error(e);
-        serverLogger.error(e)
-            .finally(() => {
-                throw e;
-            });
+        if(serverLogger){
+            serverLogger.error(e)
+                .finally(() => {
+                    throw e;
+                });
+        }
     }
 };
 
